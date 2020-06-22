79e4f3e0ee6d18751395474d851e45fab929e8de
paolo <paolomxs@gmail.com>
2020-06-21 21:18:57 -0400

    c4

--- apps/empresa/views.py
+++ apps/empresa/views.py
@@ -275,10 +275,8 @@ def get_sucurales_by_distancia(request, estado, id_ciudad):
     
     distances = ShowSucursal_Serializer(sucursales, many=True).data
     data = calcular_sucursal_distances(distances, obj.validated_data['ubicacion'])
-    p1 = json.dumps(data)
-    p2 = json.loads(p1)
-
-    return Response(p2)
+    
+    return Response(data)
 
 
 
